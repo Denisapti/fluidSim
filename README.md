5feee40cd4e9e696359fcc5dd6da46ec5fff8823
Denisapti <deni.2007.a@outlook.com>
2025-01-31 22:35:52 +0000

    initialise main file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fluidSim
+
+https://youtu.be/jis1MC5Tm8k?si=hkXchJ5Jb77dMikQ
---
+++ sim.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1000, 1000);
+    background(0);
+}
+
+function draw() {
+    fill(255, 50);
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50);
+}
+
+function windowResized() {
+    ;
+}
